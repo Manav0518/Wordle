a921cfb6407cf3f4852f825f3c60ac3433e1ce23
Manav0518 <manavpatel0154@gmail.com>
2025-09-22 11:30:06 -0400

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -1,32 +1,51 @@
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-// TODO JAVADOC
+/**
+ * A simple generic singly linked list implementation.
+ *
+ * @param <T> the type of elements in this list
+ */
 public class LinkedList<T> implements List<T> {
     private Node<T> head;
     private int size;
 
-    // TODO JAVADOC
+    /**
+     * Constructs an empty LinkedList.
+     */
     public LinkedList() {
         head = null;
         size = 0;
     }
 
-    // TODO JAVADOC
+    /**
+     * Constructs a LinkedList from an array of elements.
+     *
+     * @param data the array of elements to insert into the list
+     */
     public LinkedList(T[] data) {
         for (T datum : data) {
-            add(datum); // keep adding to end
+            add(datum);
         }
     }
 
-    // TODO JAVADOC
+    /**
+     * Returns the head node of the list.
+     *
+     * @return the head node, or null if the list is empty
+     */
     public Node<T> getHead() {
         return head;
     }
 
-    // TODO JAVADOC
+    /**
+     * Converts this linked list into an array.
+     *
+     * @return an array containing all elements in the list
+     */
     public T[] toArray() {
-        return null; // FIXME
+        // TODO: Implement this
+        return null;
     }
 
     @Override
@@ -43,73 +62,163 @@ public class LinkedList<T> implements List<T> {
             sb.append("TODO: Implement toArray method...");
         } else {
             for (int i = 0; i < data.length - 1; ++i) {
-                sb.append(String.format("%s, ", data[i])); // append all but last value
+                sb.append(String.format("%s, ", data[i]));
             }
             if (data.length > 0) {
-                sb.append(String.format("%s", data[data.length - 1])); // append last value
+                sb.append(String.format("%s", data[data.length - 1]));
             }
         }
         sb.append("]\n============================");
         return sb.toString();
     }
 
+    /**
+     * Appends the specified element to the end of the list.
+     *
+     * @param element element to be appended
+     * @throws IllegalArgumentException if the element is null
+     */
     @Override
     public void add(T element) throws IllegalArgumentException {
-        if head.getData()
+        // TODO: Implement this
     }
 
+    /**
+     * Inserts the specified element at the specified position in this list.
+     *
+     * @param index   index at which the element is to be inserted
+     * @param element element to be inserted
+     * @throws IndexOutOfBoundsException if the index is out of range
+     * @throws IllegalArgumentException  if the element is null
+     */
     @Override
-    public void add(int index, T element) throws IndexOutOfBoundsException, IllegalArgumentException {
-        // FIXME
+    public void add(int index, T element)
+            throws IndexOutOfBoundsException, IllegalArgumentException {
+        // TODO: Implement this
     }
 
+    /**
+     * Removes and returns the first element from the list.
+     *
+     * @return the removed element
+     * @throws NoSuchElementException if the list is empty
+     */
     @Override
     public T remove() throws NoSuchElementException {
-        return null; // FIXME
+        // TODO: Implement this
+        return null;
     }
 
+    /**
+     * Removes the element at the specified index.
+     *
+     * @param index index of the element to remove
+     * @return the removed element
+     * @throws NoSuchElementException    if the list is empty
+     * @throws IndexOutOfBoundsException if the index is invalid
+     */
     @Override
-    public T remove(int index) throws NoSuchElementException, IndexOutOfBoundsException {
-        return null; // FIXME
+    public T remove(int index)
+            throws NoSuchElementException, IndexOutOfBoundsException {
+        // TODO: Implement this
+        return null;
     }
 
+    /**
+     * Removes the first occurrence of the specified element from this list.
+     *
+     * @param element the element to remove
+     * @return the removed element
+     * @throws IllegalArgumentException if the element is null
+     * @throws NoSuchElementException   if the element is not found
+     */
     @Override
-    public T remove(T element) throws IllegalArgumentException, NoSuchElementException {
-        return null; // FIXME
+    public T remove(T element)
+            throws IllegalArgumentException, NoSuchElementException {
+        // TODO: Implement this
+        return null;
     }
 
+    /**
+     * Replaces the element at the specified position with the specified element.
+     *
+     * @param index   index of the element to replace
+     * @param element element to be stored
+     * @return the element previously at the specified position
+     * @throws IndexOutOfBoundsException if the index is out of range
+     * @throws IllegalArgumentException  if the element is null
+     */
     @Override
-    public T set(int index, T element) throws IndexOutOfBoundsException, IllegalArgumentException {
-        return null; // FIXME
+    public T set(int index, T element)
+            throws IndexOutOfBoundsException, IllegalArgumentException {
+        // TODO: Implement this
+        return null;
     }
 
+    /**
+     * Returns the element at the specified position.
+     *
+     * @param index index of the element to return
+     * @return the element at the specified position
+     * @throws IndexOutOfBoundsException if the index is invalid
+     */
     @Override
     public T get(int index) throws IndexOutOfBoundsException {
-        return null; // FIXME
+        // TODO: Implement this
+        return null;
     }
 
+    /**
+     * Checks whether the list contains the specified element.
+     *
+     * @param element element whose presence is to be tested
+     * @return true if the list contains the element, false otherwise
+     * @throws IllegalArgumentException if the element is null
+     */
     @Override
     public boolean contains(T element) throws IllegalArgumentException {
-        return false; // FIXME
+        // TODO: Implement this
+        return false;
     }
 
+    /**
+     * Removes all elements from the list.
+     */
     @Override
     public void clear() {
-        // FIXME
+        // TODO: Implement this
     }
 
+    /**
+     * Returns true if the list contains no elements.
+     *
+     * @return true if empty, false otherwise
+     */
     @Override
     public boolean isEmpty() {
-        return false; // FIXME
+        // TODO: Implement this
+        return false;
     }
 
+    /**
+     * Returns the number of elements in the list.
+     *
+     * @return size of the list
+     */
     @Override
     public int size() {
-        return 0; // FIXME
+        // TODO: Implement this
+        return 0;
     }
 
+    /**
+     * Returns an iterator over the elements in this list.
+     *
+     * @return an iterator
+     */
     @Override
     public Iterator<T> iterator() {
-        return null; // FIXME
+        // TODO: Implement this
+        return null;
     }
 }
